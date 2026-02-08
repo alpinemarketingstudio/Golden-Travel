21b2b203b9035e855f93047a87dd1427ae5abe6a
Dipesh Thapa Magar <thapadipesh722@gmail.com>
2026-02-08 13:02:51 +0545

    new

--- golden/src/components/Home.jsx
+++ golden/src/components/Home.jsx
@@ -52,9 +52,9 @@ export default function Home() {
     handleSearch(place);
   };
 
-  /* =========================
+  /* =================
      DATE LOGIC
-  ========================== */
+  ==================== */
   const onStartDateChange = (date) => {
     setStartDate(date);
     setEndDateError("");
